80d2555680088719c9d42eb99c14a9d9c267c287
pouria pou <pouriap024@yahoo.com>
2024-04-06 15:10:16 +0200

    update goals component for mobile screen

--- src/components/goals/index.jsx
+++ src/components/goals/index.jsx
@@ -3,8 +3,8 @@ import GoalsText from "./goalsText";
 import testImg from "../../assets/Images/5512f1de154d50572a27785e839fccc6.jpg";
 
 const Goals = () => {
-  const title1 = "Achieve more";
-  const title2 = "then your limits";
+  const title = "Achieve more then your limits";
+
   const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
   eiusmod tempor incididunt ut labore et dolore magna aliqua. Placerat
   in egestas erat imperdiet sed. Mi in nulla posuere sollicitudin
@@ -13,14 +13,15 @@ const Goals = () => {
   auctor eu augue ut lectus arcu bibendum at.`;
   return (
     <section>
-      <Grid container pt={2}>
-        <Grid container item flex={2} pl={2} pr={1} flexDirection={"column"}>
-          <GoalsText text={title1} />
-          <GoalsText text={title2} pl={5} />
-          <GoalsText text={text} variant={"subtitle"} />
+      <Grid container pt={2} flexDirection={"column"}>
+        <Grid item display={"flex"} flex={1} justifyContent={"center"}>
+          <GoalsText text={title} />
         </Grid>
-        <Grid container item flex={1} pt={2}>
-          <img src={testImg} style={{ width: "100%", objectFit: "contain" }} />
+        <Grid item flex={1} display={"flex"} justifyContent={"center"} pt={2}>
+          <img src={testImg} style={{ width: "55%", objectFit: "contain" }} />
+        </Grid>
+        <Grid item pt={2} sx={{ textAlign: "center" }}>
+          <GoalsText text={text} variant="subtitle" />
         </Grid>
       </Grid>
     </section>
