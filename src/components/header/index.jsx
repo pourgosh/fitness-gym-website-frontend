06d85c2267be71535cd7d558a051d9094a221a7f
pouria pou <pouriap024@yahoo.com>
2024-04-10 20:02:58 +0200

    update responsiveness for hero section

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,18 +1,22 @@
 import { Grid } from "@mui/material";
+import "./header.css";
 import heroImg from "../../assets/Images/headereImg.jpg";
+
 const Header = () => {
   return (
     <header>
-      <Grid container height={{ xs: 200, sm: 350, md: 550, lg: 700, xl: 900 }}>
-        <Grid item flex={1} height={"100%"}>
-          <img
-            src={heroImg}
-            aria-hidden="true"
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-          />
+      <Grid
+        container
+        height={{ xs: 200, sm: 250, between: 350, md: 400, lg: 700, xl: 1000 }}
+      >
+        <Grid
+          item
+          flex={1}
+          height={"100%"}
+          display={"flex"}
+          justifyContent={"center"}
+        >
+          <img className="heroImg" src={heroImg} aria-hidden="true" />
         </Grid>
       </Grid>
     </header>
