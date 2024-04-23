7058d6ec608b092f347d16d430a5f56f15b8d605
pouria pou <pouriap024@yahoo.com>
2024-04-23 17:53:45 +0200

    add item info with display button to singleItemPage

--- src/views/wearsPage/singleItem/index.jsx
+++ src/views/wearsPage/singleItem/index.jsx
@@ -1,14 +1,17 @@
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { productsContext } from "../../../App";
-import { useContext, useEffect } from "react";
+import { Fragment, useContext, useEffect, useState } from "react";
 import LeftSectionTop from "./leftSection";
 import RightSectionTop from "./rightSection";
+import DropDownInfo from "./dropDownInfo";
 
 const SingleWearPage = () => {
   const { wearID } = useParams();
   const product = useContext(productsContext);
   const numID = parseInt(wearID);
+  const [manufactur, setManufacture] = useState(false);
+  const [description, setDescription] = useState(false);
 
   useEffect(() => {
     product.getWearsData();
@@ -29,6 +32,39 @@ const SingleWearPage = () => {
         </Grid>
         <Grid item container flex={1} flexDirection={"column"}>
           <RightSectionTop wears={product && product.wears} numID={numID} />
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item flex={1} display={"flex"} justifyContent={"center"} pt={2}>
+          {product.wears &&
+            product.wears.map((elem) => {
+              if (elem._id === numID) {
+                return (
+                  <Fragment key={elem._id}>
+                    <Typography variant="string" color={"primary.main"}>
+                      {elem.price}€
+                    </Typography>
+                  </Fragment>
+                );
+              }
+            })}
+        </Grid>
+        <Grid
+          item
+          flex={1}
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          textAlign={"center"}
+        >
+          <DropDownInfo
+            setDescription={setDescription}
+            manufactur={manufactur}
+            setManufacture={setManufacture}
+            description={description}
+            wears={product.wears && product.wears}
+            numID={numID}
+          />
         </Grid>
       </Grid>
     </main>
